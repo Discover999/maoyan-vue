72e8f7b0d88686dcc7477f3c414e5353391c7a19
Discover999 <13189912235@163.com>
2022-11-09 23:00:54 +0800

    修复短视频样式

--- src/views/Videos/ShortVideo.vue
+++ src/views/Videos/ShortVideo.vue
@@ -8,7 +8,9 @@
         <span class="iconfont icon-liebiao2"> </span>
       </div>
     </div>
-    <div>
+
+    <!-- 短视频流 -->
+    <div class="shortlist">
       <div
         class="smvideo"
         v-for="item in ShortVideoList"
@@ -22,12 +24,15 @@
         "
       >
         <p>{{ item.title }}</p>
+
+        <!-- 用户信息 -->
         <div class="user">
           <img :src="item.user.avatarurl" alt="" />
           <div class="username">{{ item.user.nickName }}</div>
         </div>
       </div>
     </div>
+
     <!-- 底部导航栏 -->
     <tab-bar></tab-bar>
   </div>
@@ -55,13 +60,15 @@ export default {
     },
   },
   created() {
+    // 生命周期
     this.getShortVideoFun();
   },
 };
 </script>
   
-<style lang="less">
+<style lang="less" scoped>
 .videoTop {
+  // 头部样式
   display: flex;
   justify-content: space-between;
   background-color: #e54847;
@@ -78,48 +85,61 @@ export default {
     }
   }
 }
+.shortlist {
+  // 短视频列表流样式
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  box-sizing: border-box;
+  margin-bottom: 48px;
+  margin-top: -0.4%;
 
-.smvideo {
-  // background: url();
-  display: inline-block;
-  width: 49.5%;
-  height: 300px;
-  margin: 0.3% 0.015px 0 0.2px;
-  background-position: 50%;
-  background-size: cover;
-  position: relative;
-  p {
-    width: 170px;
-    margin: 0 8px;
-    position: absolute;
-    color: #fff;
-    bottom: 40px;
-    font-size: 16px;
-  }
-  .user {
-    margin: 0 8px;
-    display: flex;
-    align-items: center;
-    font-size: 12px;
-    position: absolute;
-    bottom: 10px;
-    img {
-      width: 22px;
-      height: 22px;
-      opacity: 0.9;
-      border: 1px rgba(246, 234, 197);
-      border-radius: 50%;
-      display: block;
-    }
-    .username {
-      margin: 0 6px;
-      flex: 1;
-      display: inline-block;
-      overflow: hidden;
-      white-space: nowrap;
-      text-overflow: ellipsis;
+  .smvideo {
+    // 短视频div样式
+    // background: url();
+    display: inline-block;
+    width: 49.5%;
+    height: 300px;
+    margin: 0.5% 0.5% 0 0;
+    background-position: 50%;
+    background-size: cover;
+    position: relative;
+
+    p {
+      // 标题样式
+      width: 170px;
+      margin: 0 8px;
+      position: absolute;
       color: #fff;
-      font-size: 14px;
+      bottom: 40px;
+      font-size: 16px;
+    }
+    .user {
+      // 用户信息样式
+      margin: 0 8px;
+      display: flex;
+      align-items: center;
+      font-size: 12px;
+      position: absolute;
+      bottom: 10px;
+      img {
+        width: 22px;
+        height: 22px;
+        opacity: 0.9;
+        border: 1px rgba(246, 234, 197);
+        border-radius: 50%;
+        display: block;
+      }
+      .username {
+        margin: 0 6px;
+        flex: 1;
+        display: inline-block;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        color: #fff;
+        font-size: 14px;
+      }
     }
   }
 }
