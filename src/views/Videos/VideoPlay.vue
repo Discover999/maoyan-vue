90915eaa405739398a133d618499bca24210530a
Discover999 <13189912235@163.com>
2022-11-12 22:49:38 +0800

    fix VideoPlay

--- src/views/Videos/VideoPlay.vue
+++ src/views/Videos/VideoPlay.vue
@@ -1,18 +1,37 @@
 // 视频播放页面
 <template>
-  <div>
-    <div class="vshow" v-if="VideoInfo">
-      <div
-        class="one"
-        :style="{
-          'background-image': 'url(' + VideoInfo.video.imgUrl + ')',
-        }"
-      >
-        <video src=""></video>
-      </div>
-      <div class="video-info">
-        <div class="title">{{ VideoInfo.title }}</div>
-        <div class="user">{{ VideoInfo.user.nickName }}</div>
+  <div class="main">
+    <div class="videopage">
+      <div class="vshow" v-if="VideoInfo">
+        <div class="video">
+          <video
+            class="video-play"
+            preload="auto"
+            :src="VideoInfo.video.url"
+            :poster="VideoInfo.video.imgUrl"
+            loop=""
+            controls=""
+            x5-playsinline="true"
+            playsinline="true"
+            webkit-playsinline="true"
+          />
+          <div class="topImg">
+            <img alt="loading" :src="VideoInfo.video.imgUrl" />
+            <div class="pauseIcon"></div>
+          </div>
+        </div>
+        <div class="video-info">
+          <div class="title">{{ VideoInfo.title }}</div>
+          <div class="user-info">
+            <img class="userImg" alt="" :src="VideoInfo.user.avatarurl" />
+            <div class="userName">{{ VideoInfo.user.nickName }}</div>
+            <img
+              class="icon"
+              src="//p0.meituan.net/moviemachine/5aaa710d72c78d6553be83f43bc9e4f3946.png"
+              alt=""
+            />
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -44,7 +63,7 @@ export default {
         getVideos1().then((data) => {
           this.VideoInfo = data.data.feeds[this.index];
           // console.log("索引位置 ==>", this.index);
-          // console.log("接口1返回数据 ==>", this.VideoInfo);
+          console.log("接口1返回数据 ==>", this.VideoInfo);
         });
       } else if (this.page == 2) {
         getVideos2().then((data) => {
@@ -82,46 +101,42 @@ export default {
 };
 </script>
 <style lang="less" scoped>
-div {
+.main {
   width: 100%;
-  height: 100%;
-  .vshow {
-    height: 40%;
-    width: 100%;
-    .video-info {
-      width: 100%;
-      height: 30%;
-      display: flex;
-      position: absolute;
-      bottom: 0;
-      background-color: #fff;
-      display: flex;
-      flex: 1;
-      justify-content: space-between;
-
-      .user-info {
-        margin: 0 16px;
-        display: flex;
-        align-items: center;
-        font-size: 12px;
-        bottom: 10px;
-        img {
-          width: 22px;
-          height: 22px;
-          opacity: 0.9;
-          border: 1px rgba(246, 234, 197);
-          border-radius: 50%;
-          display: block;
+  height: 100vh;
+  background: #f4f4f4;
+  .videopage {
+    .vshow {
+      .video {
+        video {
+          width: 100%;
+          position: absolute;
         }
-        .username {
-          margin: 0 6px;
-          flex: 1;
-          display: inline-block;
-          overflow: hidden;
-          white-space: nowrap;
-          text-overflow: ellipsis;
-          color: #666;
-          font-size: 14px;
+        .topImg {
+          img {
+            width: 200px;
+            height: 300px;
+            z-index: 100;
+          }
+        }
+      }
+      .video-info {
+        width: 100%;
+        background: #fff;
+        display: flex;
+        flex-direction: column;
+        padding: 14px 16px;
+        .user-info {
+          display: flex;
+          justify-content: left;
+          img {
+            width: 22px;
+            height: 22px;
+            opacity: 0.9;
+            border: 1px rgba(246, 234, 197);
+            border-radius: 50%;
+            display: block;
+          }
         }
       }
     }
