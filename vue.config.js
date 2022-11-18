773532f32f9a205e04e0b30167aaac5d577bdf69
Discover999 <13189912235@163.com>
2022-11-18 14:52:58 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // devServer: {
+  //   // 此处开启 https
+  //   https: true
+  // }
+
 })
